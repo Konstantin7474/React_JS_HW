b412125904895c45fc95631e411c1cb0e3971c43
Konstantin7474 <konstantin.duganov@mail.ru>
2024-09-19 21:16:06 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Message from './components/Message'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Message text="тест текст"/>
     </div>
   );
 }
---
+++ src/components/Message.js
@@ -0,0 +1,9 @@
+function Message({text}){
+    return(
+        <div>
+            <h1 className="message">{text}</h1>
+        </div>
+    )
+}
+
+export default Message;
